4483bd30db9fbf3272b2693cf2e3b9cafabe4961
haridandapani <haridandapani@gmail.com>
2020-07-02 12:47:05 -0400

    fixing href issue with  link sending

--- src/main/resources/static/js/selector.js
+++ src/main/resources/static/js/selector.js
@@ -131,6 +131,7 @@ function end(){
     document.getElementById("former").style.display = "block";
     document.getElementById("end").style.display = "none";
     timer = null;
+    location.href = "/"
 }
 
 function generateRandomColor(){
